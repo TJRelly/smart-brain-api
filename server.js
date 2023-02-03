8af65eef7dc862c6d32dce8b6f39275bd86fc76d
TJ <xxterrelljacksonxx@gmail.com>
2023-02-03 12:30:57 -0500

    code review

--- server.js
+++ server.js
@@ -8,20 +8,7 @@ const signin = require('./controllers/signin');
 const image = require('./controllers/image')
 const profile = require('./controllers/profile')
 
-// const postgres = require('postgres');
 require('dotenv').config();
-
-// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
-
-// const sql = postgres(URL, { ssl: 'require' });
-
-// async function getPostgresVersion() {
-//     const result = await sql`select version()`;
-//     console.log(result);
-// }
-
-// getPostgresVersion();
 
 const db = knex({
     client: 'pg',
