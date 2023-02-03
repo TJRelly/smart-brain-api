8a5690d059ccdda9601d55cc643460d999fa000f
TJ <xxterrelljacksonxx@gmail.com>
2023-02-03 07:50:31 -0500

    update database

--- server.js
+++ server.js
@@ -14,27 +14,25 @@ require('dotenv').config();
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 
-const sql = postgres(URL, { ssl: 'require' });
+const db = postgres(URL, { ssl: 'require' });
 
 async function getPostgresVersion() {
-    const result = await sql`select version()`;
+    const result = await db`select version()`;
     console.log(result);
 }
 
 getPostgresVersion();
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: PGHOST,
-        port: 5432,
-        user: PGUSER,
-        password: PGPASSWORD,
-        database: PGDATABASE
-    }
-});
-
-console.log(db)
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         host: PGHOST,
+//         port: 5432,
+//         user: PGUSER,
+//         password: PGPASSWORD,
+//         database: PGDATABASE
+//     }
+// });
 
 const app = express();
 
