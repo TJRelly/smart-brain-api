4274734f3cdc1b83dce209951e21cd21d667aac5
TJ <xxterrelljacksonxx@gmail.com>
2023-06-14 23:24:56 -0400

    added await for readability and error handling

--- controllers/image.js
+++ controllers/image.js
@@ -1,37 +1,3 @@
-const { response } = require("express");
-
-const handleAPICall = (req, res) => {
-    const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": "clarifai",
-            "app_id": "main"
-        },
-        "inputs": [
-            {
-                "data": {
-                    "image": {
-                        "url": req.body.input
-                    }
-                }
-            }
-        ]
-    });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': process.env.API_KEY
-        },
-        body: raw
-    };
-
-    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
-        .then(response => response.text())
-        .then(result => res.json(result))
-        .catch(err => res.status(400).json('unable to work with API'))
-}
-
 handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users')
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,30 +1,30 @@
-const handleSignIn = (req, res, bcrypt, db) => {
+const handleSignIn = async (req, res, bcrypt, db) => {
     const { email, password } = req.body;
     if (!email || !password) {
-        return res.status(400).json('incorrect form submission')
+      return res.status(400).json('incorrect form submission');
     }
-    db
+  
+    try {
+      const data = await db
         .select('email', 'hash')
         .from('login')
-        .where('email', '=', email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(password, data[0].hash);
-            if (isValid) {
-                return db
-                    .select('*')
-                    .from('users')
-                    .where('email', '=', email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            } else {
-                res.status(400).json('wrong credentials')
-            }
-
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-}
+        .where('email', '=', email);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+  
+      if (isValid) {
+        const user = await db
+          .select('*')
+          .from('users')
+          .where('email', '=', email);
+        res.json(user[0]);
+      } else {
+        res.status(400).json('wrong credentials');
+      }
+    } catch (err) {
+      res.status(400).json('wrong credentials');
+    }
+  };
+  
 
 module.exports = {
     handleSignIn
