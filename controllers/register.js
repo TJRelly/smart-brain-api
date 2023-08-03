4067c1409c2ad6f7426d4616021d26cfdb4680b4
TJ <xxterrelljacksonxx@gmail.com>
2023-08-03 08:06:52 -0400

    fixed functions

--- controllers/register.js
+++ controllers/register.js
@@ -1,35 +1,38 @@
 const handleRegister = (req, res, bcrypt, db) => {
-  const { email, name, password } = req.body
+  const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json("incorrect form submission")
+      return res.status(400).json('incorrect form submission')
   }
-  const hash = bcrypt.hashSync(password)
+  const hash = bcrypt.hashSync(password);
   console.log(hash)
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
+  db.transaction(trx => {
+      trx.insert({
+          hash: hash,
+          email: email
       })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
+          .into('login')
+          .returning('email')
+          .then(loginEmail => {
+              return trx('users')
+                  .returning('*')
+                  .insert({
+                      email: loginEmail[0].email,
+                      name: name,
+                      joined: new Date()
+                  })
+                  .then(user => {
+                      res.json(user[0]);
+                  })
           })
-          .then((user) => {
-            res.json(user[0])
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-  }).catch((err) => {
-    res.status(400).json("unable to register")
+          .then(trx.commit)
+          .catch(trx.rollback)
   })
+
+      .catch(err => {
+          res.status(400).json('unable to register')
+      })
 }
 
-module.exports = handleRegister
+module.exports = {
+  handleRegister
+}
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,25 +1,31 @@
 const handleSignIn = async (req, res, bcrypt, db) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json("incorrect form submission")
+    return res.status(400).json('incorrect form submission');
   }
 
   try {
     const data = await db
-      .select("email", "hash")
-      .from("login")
-      .where("email", "=", email)
-    const isValid = bcrypt.compareSync(password, data[0].hash)
+      .select('email', 'hash')
+      .from('login')
+      .where('email', '=', email);
+    const isValid = bcrypt.compareSync(password, data[0].hash);
 
     if (isValid) {
-      const user = await db.select("*").from("users").where("email", "=", email)
-      res.json(user[0])
+      const user = await db
+        .select('*')
+        .from('users')
+        .where('email', '=', email);
+      res.json(user[0]);
     } else {
-      res.status(400).json("wrong credentials")
+      res.status(400).json('wrong credentials');
     }
   } catch (err) {
-    res.status(400).json("wrong credentials")
+    res.status(400).json('wrong credentials');
   }
-}
+};
 
-module.exports = handleSignIn
+
+module.exports = {
+    handleSignIn
+}
