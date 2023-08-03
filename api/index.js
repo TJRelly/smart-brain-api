453b5af1f9e28cb4026699072c7e1a09e1b1c60d
TJ <xxterrelljacksonxx@gmail.com>
2023-08-03 06:38:03 -0400

    database update

--- api/index.js
+++ api/index.js
@@ -1,3 +1,48 @@
-const app = require("../server")
+const express = require("express")
+const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt-nodejs")
+const cors = require("cors")
+const knex = require("knex")
+const register = require("./controllers/register")
+const signin = require("./controllers/signin")
+const image = require("./controllers/image")
 
-module.exports = app
+require("dotenv").config()
+
+const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+})
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send(JSON.stringify({ message: "it's working" }))
+})
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db)
+})
+
+app.post("/signin", (req, res) => {
+  signin.handleSignIn(req, res, bcrypt, db)
+})
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, bcrypt, db)
+})
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db)
+  console.log(req)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
+})
