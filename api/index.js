c9386fe035548ec4a55344d76bb748aee216078f
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2024-03-29 13:37:07 -0400

    update middleware

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-allowCors(app)
+app.use(allowCors)
 
 app.post("/signin", (req, res) => {
   signin.handleSignIn(req, res, bcrypt, db);
